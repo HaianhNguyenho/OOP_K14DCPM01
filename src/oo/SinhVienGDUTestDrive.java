878593268ccaefa9f21936fb85cb4dbc075192fb
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2022-01-13 15:57:06 +0700

    nop bai

---
+++ src/oo/SinhVienGDUTestDrive.java
@@ -0,0 +1,12 @@
+package oo;
+
+public class SinhVienGDUTestDrive {
+    public static void main(String[] args) {
+        SinhVienGDU sinhVien;
+
+        sinhVien = new SinhVienGDU();
+
+        sinhVien.InforSV();
+        sinhVien.show();
+    }
+}
