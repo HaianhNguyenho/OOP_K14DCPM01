99a2cc93dac2802f2fa0be16bb7cc50a30a13c26
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2022-01-17 22:54:30 +0700

    update bài cũ

---
+++ src/oo/Student.java
@@ -0,0 +1,24 @@
+package oo;
+
+public class Student {
+
+    //attribute-data-state
+    boolean gioiTinh;
+    float tuoi;
+    String name;
+    String khoa;
+    double diemTB;
+
+    //method - functions - bahavior
+    
+    void hoc(){
+        System.out.println("Hoc ...");
+
+    }
+    void thi(){
+        System.out.println("Thi...");
+    }
+    void dangKyMonHoc(){
+        System.out.println();
+    }
+}
---
+++ src/oo/StudentTestDrive.java
@@ -0,0 +1,15 @@
+package oo;
+
+public class StudentTestDrive {
+    public static void main(String[] args) {
+        Student tuanStudent = new Student();
+        tuanStudent.hoc();
+
+        Student ducStudent = new Student();
+        ducStudent.thi();
+
+        Student xyzStudent = new Student();
+        xyzStudent.dangKyMonHoc();
+    }
+    
+}
