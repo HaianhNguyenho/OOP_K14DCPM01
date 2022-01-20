f68536d863c3190ee3a908a9a8df09d8fa9aa2ff
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2022-01-20 11:58:14 +0700

    BT demo constructor+this

---
+++ src/com/anhnhh2008110020/tuan03/MyIntegerTestDrive.java
@@ -0,0 +1,13 @@
+package com.anhnhh2008110020.tuan03;
+
+public class MyIntegerTestDrive {
+    public static void main(String[] args) {
+        MyInteger counter = new MyInteger();
+         
+        
+
+     counter.increment().increment().increment(); //tăng
+        
+    }
+    
+}
