b4fd6b28f9fc9059b978695e37aac024e490f670
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2022-02-24 13:09:19 +0700

    update encapsulation for Game

---
+++ src/com/anhnhh2008110020/tuan05/GameLaunch.java
@@ -0,0 +1,10 @@
+package com.anhnhh2008110020.tuan05;
+
+public class GameLaunch {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.starGame();
+    }
+    
+    
+}
---
+++ src/com/anhnhh2008110020/tuan05/Player.java
@@ -0,0 +1,27 @@
+package com.anhnhh2008110020.tuan05;
+
+public class Player {
+     //attribute
+    private int number =0 ;
+
+    
+
+     public int getNumber() {
+        return number;
+    }
+
+
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+
+    //method
+     public void guess(){
+         number = (int) (Math.random() * 10);
+         System.out.println("In'm guessting "+" "+ number); 
+     }
+    
+}
